8cc3a7de1cfafcd85f01a0ec5bf4384f6d4e0ee3
chetverninart <chetverninart@gmail.com>
2021-01-04 11:56:09 +0300

    Initial commit

---
+++ src/main/java/TaskHibernate/dao/UserDao.java
@@ -0,0 +1,18 @@
+package TaskHibernate.dao;
+
+import TaskHibernate.model.User;
+import java.util.List;
+
+public interface UserDao {
+    void createUsersTable();
+
+    void dropUsersTable();
+
+    void saveUser(String name, String lastName, byte age);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    void cleanUsersTable();
+}
